c9a87a876ac773c037fcfbbb6d3e1242dcb86605
chintp2308 <nphuongchi23@gmail.com>
2025-07-06 12:14:38 +0700

    updateShoppingCart

--- sampleshoppingcart/src/components/CartIcon.tsx
+++ sampleshoppingcart/src/components/CartIcon.tsx
@@ -10,19 +10,17 @@ export default function CartIcon() {
 
   const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
 
-  const handleToggle = () => setOpen((prev) => !prev);
-
   return (
-    <div className={styles.cartWrapper}>
-      <button onClick={handleToggle} className={styles.cartBtn}>
+    <div
+      className={styles.cartWrapper}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+    >
+      <button className={styles.cartBtn}>
         <BsCart4 /> ({totalItems})
       </button>
-      {open && (
-        <>
-          <div className={styles.overlay} onClick={handleToggle}></div>
-          <CartDropdown />
-        </>
-      )}
+
+      {open && <CartDropdown />}
     </div>
   );
 }
